fc1629658994c70f4fc3e734edea7afcb8552de9
Jeong-Heon YANG <yang@jhyang.xyz>
2022-01-17 17:06:00 +0100

    fixed menu design

--- components/ManualMenu/ChapterItem.vue
+++ components/ManualMenu/ChapterItem.vue
@@ -1,11 +1,11 @@
 <template>
   <div @click="isCollapsed = !isCollapsed">
-    <div class='uppercase border-b-1 border-yellow pl-4 h-20 flex items-center justify-between'>
+    <div class='uppercase border-b-1 border-yellow pl-4 flex items-center justify-between'>
       <span class="font-bold">{{chapter.title }}</span>
       <span v-show="isCollapsed" class="mdi mdi-chevron-down text-[2.8rem]"></span>
       <span v-show="!isCollapsed" class="mdi mdi-chevron-up text-[2.8rem]"></span>
     </div>
-    <ul v-if="!isCollapsed" class='p-4'>
+    <ul v-if="!isCollapsed" class='p-4 leading-[3.2rem]'>
       <ManualMenuArticleItem v-for='article in chapter.articles' :key='article.id' :article='article' :manual='manual'/>
     </ul>
   </div>
