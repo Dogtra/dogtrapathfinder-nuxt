93cd52ef714b4d07bf06fda23936e4b994cf5a50
Jeong-Heon YANG <yang@jhyang.xyz>
2022-01-26 02:48:29 +0100

    Added clickable links

--- components/Search/SearchResultItem.vue
+++ components/Search/SearchResultItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div :key="item.id">
-    <p class="text-18 font-bold" v-dompurify-html="item._highlightResult.title.value"></p>
-    <p class="text-14 font-normal text-[#B4B4B4] pt-4 pb-2">{{ manual.title }} > <span v-dompurify-html='chapter.title'></span></p>
+    <NuxtLink class="text-18 font-bold" v-dompurify-html="item._highlightResult.title.value" :to='articleUrl'></NuxtLink>
+    <p class="text-14 font-normal text-[#B4B4B4] pt-4 pb-2"><NuxtLink :to='manualUrl'>{{ manual.title }}</NuxtLink> > <span v-dompurify-html='chapter.title'></span></p>
     <p class="line-clamp-6 md:line-clamp-4 font-normal mb-12" v-dompurify-html="item._highlightResult.content.value"></p>
   </div>
 </template>
@@ -19,6 +19,12 @@ export default {
     }
   },
   computed: {
+    manualUrl() {
+      return '/manuals/' + this.manual.slug
+    },
+    articleUrl() {
+      return this.manualUrl + '/' + this.item.uuid
+    },
     manual() {
       return this.manuals.find(manual =>
         parseInt(manual.id) === this.item.chapter.manual
