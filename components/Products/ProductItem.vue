37ab23a1246b17899b8c320cfe63d21025f0a1f6
Jeong-Heon YANG <yang@jhyang.xyz>
2022-02-01 22:16:34 +0100

    desktop products

--- components/Products/ProductItem.vue
+++ components/Products/ProductItem.vue
@@ -1,31 +1,43 @@
 <template>
-  <div class='mt-10 md:mt-12 bg-white p-0 md:px-24 md:py-12 rounded-box flex overflow-hidden'>
-    <div>
-      <div class='w-29rem'>
-        <img :src='imageUrl'>
+  <div class='mt-10 md:mt-12 bg-white p-0 md:px-24 md:py-12 rounded-box md:rounded-3xl flex overflow-hidden shadow-xl'>
+    <div class='md:pr-24'>
+      <div class='w-full md:w-[29rem]'>
+        <img class='w-full' :src='imageUrl'>
       </div>
       <div class='block md:hidden pt-8 p-6'>
         <div class='uppercase flex justify-between items-center pb-6 font-bold border-b-1 border-solid border-[#888888]'>
           <p class='text-18'>{{ product.title }}</p>
-          <div>
-            <div class='border-solid border-1 border-black rounded-full text-12 flex items-center h-10 pl-4'>
-              Shop now<span class='mdi mdi-chevron-right text-22'></span>
-            </div>
+          <div class='border-solid border-1 border-black rounded-full text-12 flex items-center h-10 pl-4'>
+            Shop now<span class='mdi mdi-chevron-right text-22'></span>
           </div>
         </div>
-        <div class='flex py-6'>
-          <div>ratings</div>
-          <div>{{ product.price }}</div>
+        <div class='flex py-6 flex justify-between'>
+          <p>ratings</p>
+          <p>$ {{ product.price }}</p>
         </div>
         <div>
           <template v-for='feature in product.features'>
-            {{feature}}
+            {{ feature.title }}
           </template>
         </div>
       </div>
     </div>
     <div class='hidden md:block'>
-      {{ product }}
+      <p class='text-24 uppercase font-bold'>{{ product.title }}</p>
+      <div class='flex justify-between border-[#C5C5C5] border-b-1 border-solid pb-6'>
+        <div>ratings</div>
+        <div>ratings link</div>
+      </div>
+      <p class='text-14 pt-4'>$ {{ product.price }}</p>
+      <p class='pt-4 pb-6'>{{ product.description }}</p>
+      <div>
+        <template v-for='feature in product.features'>
+          {{ feature.title }}
+        </template>
+      </div>
+      <div class='border-solid border-1 border-black rounded-full text-12 flex items-center h-10 pl-4 w-fit float-right font-bold uppercase'>
+        Shop now<span class='mdi mdi-chevron-right text-22'></span>
+      </div>
     </div>
   </div>
 </template>
