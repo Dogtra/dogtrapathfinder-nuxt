a58191ec33dd8f397aa13694f5764d7e6d0eb840
Jeong-Heon YANG <yang@jhyang.xyz>
2022-02-04 14:46:30 -0800

    Added proper slider on popular articles

--- components/Banner/PopularArticleItem.vue
+++ components/Banner/PopularArticleItem.vue
@@ -1,11 +1,15 @@
 <template>
-  <NuxtLink :to='articleUrl' class='shadow-xl bg-white border-4 border-black border-solid rounded-lg py-8 md:py-10 px-4 md:px-8 min-w-[20rem] md:min-w-[24.5rem] max-w-[20rem] md:max-w-[24.5rem] w-[20rem] md:w-[24.5rem]'>
+  <div class='shadow-xl bg-white border-4 border-black border-solid rounded-lg h-full min-w-[20rem] md:min-w-[24.5rem] max-w-[20rem] md:max-w-[24.5rem] w-[20rem] md:w-[24.5rem] mx-4'>
+    <NuxtLink :to='articleUrl' class='h-full flex flex-col justify-between py-8 md:py-10 px-4 md:px-8'>
+      <div>
     <span
-        class="article-slide-tag rounded-full bg-black text-yellow flex justify-center items-center h-10 w-fit px-4 text-14 uppercase"><strong>{{ article.chapter.manual.tag }}</strong></span>
-    <p class="article-slide-title pt-4 pb-2 text-18 md:text-20"><strong>{{ article.title }}</strong></p>
-    <p class="line-clamp-3">{{ article.content_plain_text }}</p>
-    <div class="text-right text-14 underline pt-4"><NuxtLink :to="articleUrl">Read more...</NuxtLink></div>
-  </NuxtLink>
+      class="article-slide-tag rounded-full bg-black text-yellow flex justify-center items-center h-10 w-fit px-4 text-14 uppercase"><strong>{{ article.chapter.manual.tag }}</strong></span>
+        <p class="article-slide-title pt-4 pb-2 text-18 md:text-20"><strong>{{ article.title }}</strong></p>
+        <p class="line-clamp-3">{{ article.content_plain_text }}</p>
+      </div>
+      <div class="text-right text-14 underline pt-4"><NuxtLink :to="articleUrl">Read more...</NuxtLink></div>
+    </NuxtLink>
+  </div>
 </template>
 <script>
 export default {
--- components/Banner/PopularArticlesBanner.vue
+++ components/Banner/PopularArticlesBanner.vue
@@ -5,10 +5,16 @@
       <div class='cursor-pointer min-w-[3rem] min-h-[3rem] rounded-full flex justify-center items-center bg-gray-200 hover:bg-gray-300' @click='scrollLeft'>
         <span class="mdi mdi-chevron-left"></span>
       </div>
-      <div ref='slider' class='articles-sliders overflow-x-auto flex px-8 gap-12 pb-20 w-fit'>
-        <template v-for='article in popularArticles'>
-          <PopularArticleItem :key="article.id + index" :article="article"/>
-        </template>
+      <div ref='slider' class='flex gap-12 pb-20 min-w-0'>
+        <VueSlickCarousel
+          v-if='popularArticles.length'
+          ref='c1'
+          v-bind='settings'
+        >
+          <template v-for='article in popularArticles'>
+            <PopularArticleItem :key="article.id + index" :article="article"/>
+          </template>
+        </VueSlickCarousel>
       </div>
       <div class='cursor-pointer min-w-[3rem] min-h-[3rem] rounded-full flex justify-center items-center bg-gray-200 hover:bg-gray-300' @click='scrollRight'>
         <span class="mdi mdi-chevron-right"></span>
@@ -25,7 +31,48 @@ export default {
   components: {PopularArticleItem},
   data() {
     return {
-      popularArticles: []
+      popularArticles: [],
+    }
+  },
+  computed: {
+    settings() {
+      return {
+      "arrows": false,
+      "slidesToShow": Math.min(5, this.popularArticles.length),
+      "slidesToScroll": Math.min(5, this.popularArticles.length),
+      "focusOnSelect": true,
+      "responsive": [
+        {
+          "breakpoint": 1398,
+          "settings": {
+            "slidesToShow": Math.min(4, this.popularArticles.length),
+            "slidesToScroll": Math.min(4, this.popularArticles.length),
+            "initialSlide": 4
+          }
+        },
+        {
+          "breakpoint": 1070,
+          "settings": {
+            "slidesToShow": Math.min(3, this.popularArticles.length),
+            "slidesToScroll": Math.min(3, this.popularArticles.length)
+          }
+        },
+        {
+          "breakpoint": 809,
+          "settings": {
+            "slidesToShow": Math.min(2, this.popularArticles.length),
+            "slidesToScroll": Math.min(2, this.popularArticles.length)
+          }
+        },
+        {
+          "breakpoint": 472,
+          "settings": {
+            "slidesToShow": Math.min(1, this.popularArticles.length),
+            "slidesToScroll": Math.min(1, this.popularArticles.length)
+          }
+        },
+      ]
+    }
     }
   },
   async mounted() {
@@ -52,3 +99,22 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+>>> .slick-track {
+  display: flex;
+}
+
+>>> .slick-list {
+  overflow: hidden;
+}
+
+>>> .slick-slider {
+  max-width: 100%;
+}
+
+>>> .slick-slide {
+  display: flex;
+  justify-content: center;
+}
+</style>
