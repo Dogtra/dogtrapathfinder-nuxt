9801518474f4b1816c134458bf0730bc08518dcb
Jeong-Heon YANG <yang@jhyang.xyz>
2022-02-16 08:13:10 -0800

    Added order and proper banner

--- components/Banner/FooterBanner.vue
+++ components/Banner/FooterBanner.vue
@@ -2,26 +2,16 @@
   <div id='footer-banner'
        class='bg-cover bg-center h-auto text-white object-fill justify-center text-center flex-col py-10 md:py-20'
        :style='footerBannerStyle'>
-    <div class='flex justify-center gap-[2.6rem]'>
+    <p class='pt-4 text-20'>DOWNLOADING THE APP</p>
+    <div class='flex justify-center flex-col md:flex-row'>
       <template v-for='app in filteredApps'>
         <FooterBannerImage :key='app.icon.url' :app='app' />
       </template>
     </div>
-    <p class='pt-4'>DOWNLOADING THE</p>
-    <p class='pb-4'>
-      <span class='font-extrabold'>PATHFINDER </span>
-      <template v-if='filteredApps.length > 1'>
-        or <span class='font-extrabold'>PATHFINDER2</span>
-      </template>
-      APP
-    </p>
-    <div class='w-[16.6rem] md:w-[20rem] mx-auto flex gap-4'>
-      <div class='w-1/2'><a :href='filteredApps[0].android_store_url'><img alt='Get it on Google Play' src='~/assets/images/mobile-app-android-white.webp'/></a></div>
-      <div class='w-1/2'><a class='max-w-full' :href="filteredApps[0].apple_store_url"><img src="~/assets/images/mobile-app-apple-white.webp" alt="Download on the App Store"></a></div>
-    </div>
   </div>
 </template>
 <script>
+import { orderBy } from 'lodash'
 import { strapiMediaUrl } from '~/utils/strapi'
 import FooterBannerImage from '~/components/Banner/FooterBannerImage'
 
@@ -48,7 +38,7 @@ export default {
       return {}
     },
     filteredApps() {
-      return this.apps.filter(app => !app.manual.hidden)
+      return orderBy(this.apps.filter(app => !app.manual.hidden), 'order')
     },
   },
 }
--- components/Banner/FooterBannerImage.vue
+++ components/Banner/FooterBannerImage.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <img class='w-16 md:w-[5.2rem] h-16 md:h-[5.2rem]' :src='appIconUrl'>
+  <div class='footer-banner-item px-8 py-8 w-fit mx-auto md:mx-0'>
+    <img class='w-16 md:w-[5.2rem] h-16 md:h-[5.2rem] mx-auto my-4' :src='appIconUrl'>
+    <p class='pb-4'>
+      <span class='font-extrabold'> {{ app.name }} </span> APP
+    </p>
+    <div class='w-[16.6rem] md:w-[30rem] mx-auto flex gap-4'>
+      <div class='w-1/2'><a :href='app.android_store_url'><img alt='Get it on Google Play' src='~/assets/images/mobile-app-android-white.webp'/></a></div>
+      <div class='w-1/2'><a class='max-w-full' :href="app.apple_store_url"><img src="~/assets/images/mobile-app-apple-white.webp" alt="Download on the App Store"></a></div>
+    </div>
   </div>
 </template>
 <script>
@@ -21,3 +28,18 @@ export default {
   }
 }
 </script>
+
+<style>
+.footer-banner-item + .footer-banner-item{
+  border-left: 0;
+  border-top: 1px solid white;
+}
+
+@media (min-width: 768px) {
+  .footer-banner-item + .footer-banner-item{
+    border-left: 1px solid white;
+    border-top: 0;
+  }
+}
+
+</style>
