be25dca237b8e4781c9a6397feb4bd8790759c4c
Jeong-Heon YANG <yang@jhyang.xyz>
2022-03-23 14:10:01 -0700

    Replaced with proper icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -110,22 +110,12 @@ export default {
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
+    icon: {
+      source: 'static/android-chrome-512x512.png'
+    },
     manifest: {
       'name': 'Dogtra Pathfinder',
       'short_name': 'Pathfinder',
-      'icons':
-        [
-          {
-            'src': '/android-chrome-192x192.png',
-            'sizes': '192x192',
-            'type': 'image/png'
-          },
-          {
-            'src': '/android-chrome-512x512.png',
-            'sizes': '512x512',
-            'type': 'image/png'
-          }
-        ],
       'theme_color': '#FFDD00',
       'background_color': '#000000',
       'display': 'standalone'
